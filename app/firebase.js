b7306163fe9e6148251aa9ad49f7141d5984814c
Bushra <107013084+bushra-thepixelatedonut@users.noreply.github.com>
2022-06-11 12:37:01 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -2,14 +2,15 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+// fill your own credentials
 const firebaseConfig = {
-    apiKey: "AIzaSyAghCeRTTiHBMeJVMUJEV-f7InZX5uExMg",
-    authDomain: "netflix-clone-8741d.firebaseapp.com",
-    projectId: "netflix-clone-8741d",
-    storageBucket: "netflix-clone-8741d.appspot.com",
-    messagingSenderId: "938067075623",
-    appId: "1:938067075623:web:84e49ee7f950ee419ce402",
-    measurementId: "G-XRH86PQH05"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 let app;
